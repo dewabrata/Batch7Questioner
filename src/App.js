3c35aeab4a1f33616384e07544036f8b025478ec
dewabrata <dewabrata@gmail.com>
2021-01-28 11:27:08 +0700

    add screen about us

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react'
 import Header from './component/Header'
 import FormPertanyaan from './component/FormPertanyaan'
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 export default class App extends Component {
   render() {
     return (
